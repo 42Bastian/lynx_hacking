9408dfc98f10919643a09c975f9fd8fa923bc12f
42Bastian <bs42@monlynx.de>
2020-02-19 08:17:43 +0100

    remove stuff not needed

--- cycle_check_hbl/cycle_check.asm
+++ cycle_check_hbl/cycle_check.asm
@@ -11,40 +11,25 @@
 DEBUG		set 1
 Baudrate	set 62500
 
-_1000HZ_TIMER	set 7
-
 IRQ_SWITCHBUF_USR set 1
 
 	include <includes\hardware.inc>
 ****************
-	MACRO DoSWITCH
-	dec SWITCHFlag
-.\wait_vbl
-	bit SWITCHFlag
-	bmi .\wait_vbl
-	ENDM
-
-****************
 * macros
 	include <macros/help.mac>
 	include <macros/if_while.mac>
-	include <macros/font.mac>
 	include <macros/mikey.mac>
 	include <macros/suzy.mac>
 	include <macros/irq.mac>
-	include <macros/newkey.mac>
 	include <macros/debug.mac>
 ****************
 * variables
 	include <vardefs/debug.var>
 	include <vardefs/help.var>
-	include <vardefs/font.var>
 	include <vardefs/mikey.var>
 	include <vardefs/suzy.var>
 	include <vardefs/irq.var>
-	include <vardefs/newkey.var>
 	include <vardefs/serial.var>
-	include <vardefs/1000Hz.var>
 ****************************************************
 
  BEGIN_ZP
@@ -74,16 +59,13 @@ Start::
 	INITSUZY
 	SETRGB pal
 	INITIRQ irq_vektoren
-	INITKEY
-	INITFONT LITTLEFNT,RED,WHITE
 	FRAMERATE 75
 
 	jsr InitComLynx
 
 	SETIRQ 2,VBL
 	SETIRQ 0,HBL
-	SCRBASE screen0,screen1
-	SET_MINMAX 0,0,160,102
+	SCRBASE screen0
 
 	lda #$c0
 	ora _SPRSYS
@@ -179,41 +161,17 @@ cnt set cnt+1
 	dc.w dummy
 	endr
 
-
-
 ****************
 VBL::
 	ldx #0
 	stz $fda0
-	IRQ_SWITCHBUF
 	END_IRQ
-****************
-_cls::	lda #<clsSCB
-	ldy #>clsSCB
-	jmp DrawSprite
-
-clsSCB
-	dc.b $0,$10,0
-	dc.w 0,clsDATA
-	dc.w 0,0
-	dc.w $100*10,$100*102
-clsCOLOR
-	dc.b $00
-clsDATA
-	dc.b 2,%01111100
-	dc.b 0
 
 ****************
 * INCLUDES
-	include <includes/draw_spr.inc>
 	include <includes/irq.inc>
-	include <includes/1000Hz.inc>
 	include <includes/serial.inc>
-	include <includes/font.inc>
-	include <includes/font2.hlp>
-	include <includes/newkey.inc>
 	include <includes/debug.inc>
-	include <includes/hexdez.inc>
 	align 2
 
 pal
