bd3ee127b78f406edbb6ce2bd7735927addbe43b
42Bastian <bs42@monlynx.de>
2021-08-24 12:22:47 +0100

    split

--- cycle_check_hbl/cycle_check.asm
+++ cycle_check_hbl/cycle_check.asm
@@ -64,7 +64,7 @@ Start::
 	jsr InitComLynx
 
 	SETIRQ 2,VBL
-	SETIRQ 0,HBL
+//->	SETIRQ 0,HBL
 	SCRBASE screen0
 
 	lda #$c0
@@ -72,75 +72,115 @@ Start::
 	sta SPRSYS
 	sta _SPRSYS
 
+	ldx	#1
 	cli
-
 main:
-	nop
-	bra	main
+	stz	$fda0
+	stz	$fdb0
+	cpx 	#0
+	bne	main
+	ldy	#10
+w0:
+	lda	$fd00
+w1:	cmp 	$fd02
+	bne	w1
+w2:	cmp	$fd02
+	beq	w2
+	dec	$fdb0
 
-	ALIGN	256
+	REPT 10
+	dc.b $5c,0,0
+	ENDR
+
+	stz	$fdb0
+	dey
+	bne	w0
+	bra	main
+//->	ALIGN	256
 HBL:
 	inx
 	inx
 	jmp	(test,x)
 
 dummy:
-	dec $FDA0
+	dec $FDa0
+empty:
 	stz $fda0
-	END_IRQ
+	stz $fdb0
+	jmp w0
 
 	MACRO defTest ; rept opcode
 	ALIGN	256
 test_\0:
-	dec $FDA0
+	dec $FDa0
 	REPT \1
 	\2
 	ENDR
-	stz $FDA0
-	END_IRQ
+	stz $fda0
+	stz $fdb0
+	jmp w0
 	ENDM
 
-	defTest a,32,dc.b $1b
-	defTest b,64,dc.b $1b
-	defTest c,96,dc.b $1b
-	defTest d,128,dc.b $1b
-	defTest e,32,nop
-	defTest f,32,adc $ff
-	defTest g,32,inc $ff
-	defTest h,32,inc $8000
+reptCnt	set 8
+
+	defTest a,reptCnt,dc.b $1b
+	defTest b,2*reptCnt,dc.b $1b
+	defTest c,3*reptCnt,dc.b $1b
+	defTest d,4*reptCnt,dc.b $1b
+	defTest e,5*reptCnt,dc.b $1b
+	defTest f,6*reptCnt,dc.b $1b
+	defTest g,7*reptCnt,dc.b $1b
+	defTest h,8*reptCnt,dc.b $1b
+	defTest i,9*reptCnt,dc.b $1b
+
+	defTest j,reptCnt,adc $ff
+	defTest k,reptCnt,inc $ff
+	defTest l,reptCnt,{dc.b $f4,0}
+	defTest m,reptCnt,{dc.b $5c,0,0}
+	defTest n,reptCnt,nop
 
 	MACRO entry ; test
-	rept 8
+	rept 5
 	dc.w test_\0
 	endr
-cnt set cnt+8
+	dc.w empty
+cnt set cnt+6
 	ENDM
 
 cnt set 1
 test:
-	dc.w 0
-	entry a
-	entry b
-	entry c
-	entry d
-	entry e
-	entry f
-	entry g
-	entry h
+	dc.w empty
+	rept 40
+	dc.w empty
+	endr
+//->	entry a
+//->	entry b
+//->	entry c
+//->	entry d
+//->	entry e
+//->	entry f
+//->	entry g
+//->	entry h
+//->	entry i
+//->	entry m
+//->	entry k
+//->	entry l
+//->	entry m
+//->	entry n
 
 	rept 107-cnt
-	dc.w dummy
+	dc.w main
 	endr
 
 ****************
 VBL::
 	ldx #0
-	stz $fda0
 	END_IRQ
 
 ****************
 * INCLUDES
-	include <includes/irq.inc>
+//->	include <includes/irq.inc>
+	include "my_irq.inc"
 	include <includes/serial.inc>
 	include <includes/debug.inc>
 	align 2
