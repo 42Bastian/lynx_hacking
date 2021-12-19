f5da7fb006fa08c34e2cee25199b9b95229e5b87
42Bastian <bs42@monlynx.de>
2021-12-19 12:31:28 +0000

    add LNX only version of monalynx

---
+++ monalynx_no_o/monaload.asm
@@ -0,0 +1,71 @@
+***************
+* Lynx port of "MONA"
+* Loader for LNX
+****************
+
+	include <includes/hardware.inc>
+	include <macros/help.mac>
+
+	include "monalynx.equ"
+
+;;; ROM sets this address
+screen0	 equ $2000
+
+ BEGIN_ZP
+path_length	ds 1
+plot_color	ds 1
+plot_x		ds 1
+plot_y		ds 1
+seed		ds 4
+dir		ds 1
+tmp		ds 1
+ptr		ds 2
+
+ END_ZP
+
+	RUN	$200
+StartLoad::
+//->	ldy	#2		// y = 2 from ROM
+.pal
+	lda	colors_g,y
+	sta	$fda1,y
+	lda	colors_br,y
+	sta	$fdb1,y
+	dey
+	bpl	.pal
+.load
+	iny
+	lda	RCART0
+	sta	(5),y		; ptr used by ROM
+	cpy	#brush-Start-1
+	bne	.load
+
+	;; Code moved from monalynx.asm to fill up to 49
+//->	lda	#$7e	// loaded from card
+	sta	seed+3
+	lda	#$c8
+	sta	seed+2
+	stz	MATHE_E
+	ldy	#63
+main:
+	sty	plot_color
+	lda	RCART0
+	sta	seed
+	sta	plot_x
+	;; constants do no harm if executed, so no jump over
+colors_g::
+	dc.b $06		; => ASL $A
+colors_br::
+	dc.b $0a
+	dc.b $4e		; => LSR $008e
+	dc.b $8e
+End::
+size	set End-StartLoad
+free	set 49-size
+
+echo "Free in loader: %Dfree"
+
+	IF free != 0
+	echo "Size must be == 49!"
+	ENDIF
+	dc.b	$00		; end mark!
---
+++ monalynx_no_o/monalynx.asm
@@ -0,0 +1,135 @@
+***************
+* Lynx port of "MONA"
+* Main routine
+****************
+
+	include <includes/hardware.inc>
+	include <macros/help.mac>
+
+ IFND LNX
+ error "No more .o"
+ ENDIF
+
+;;; ROM sets this address
+screen0	 equ $2000
+
+ BEGIN_ZP
+path_length	ds 1
+plot_color	ds 1
+plot_x		ds 1
+plot_y		ds 1
+seed		ds 4
+dir		ds 1
+tmp		ds 1
+ptr		ds 2
+
+ END_ZP
+
+	run $232		; first byte after decrypted header
+
+main	equ $224		; check with monaload.equ !
+
+Start::
+	lda	RCART0
+	sta	seed+1
+	sta	plot_y
+	phy
+;;;------------------------------
+.loop0
+	smb5	path_length	; path_length = 32 (zp 0 == 0 after ROM !)
+.loop
+	asl	seed
+	rol	seed+1
+	rol	seed+2
+	rol	seed+3
+	bcc	.noxor
+
+	ldx	#3
+.l1
+	lda	mask,x
+	eor	seed,x
+	sta	seed,x
+	dex
+	bpl	.l1
+	sta	dir
+.noxor:
+	;; next pixel position
+	ldx	#0
+	bbs1	dir,.x		; bit 2 set => x, else y
+	inx
+.x
+	bbs7	dir,.minus	; bit 7 set => decrement else increment
+	inc	plot_x,x
+	dc.b	$AD		; Opcode: LDA nn => skip "dec plot_x,x"
+.minus
+	dec	plot_x,x
+
+	rmb7	plot_x		; == and #$7f
+	rmb7	plot_y
+
+	;; calculate byte position
+	lda	plot_y
+	cmp	#96		; brushes are calculated for 128x96!
+	bcs	.skip
+
+	sta	MATHE_C
+	lda	#80
+	sta	MATHE_E+1
+.ws
+	bit	SPRSYS
+	bmi	.ws
+
+	lda	plot_x
+	lsr			; x => byte position
+	clc
+	adc	MATHE_A+1
+	sta	ptr
+	lda	MATHE_A+2
+	adc	#$20
+	sta	ptr+1		; ptr => byte
+
+	;; set pixel
+	ldx	#$f0
+	lda	plot_color
+	and	#3		; only 0..3
+	bbs0	plot_x,.1	; odd pixel ? (== low nibble)
+	asl
+	asl
+	asl
+	asl
+	ldx	#$f
+.1
+	sta	tmp		; save shifted color
+	txa			; get mask
+	and	(ptr)		; preserve other pixel
+	ora	tmp		; get color
+	sta	(ptr)		; store both
+.skip
+	dec	path_length
+	bne	.loop
+	dey
+	bpl	.loop0
+	ply
+	dey
+.done
+	bmi	.done
+	jmp	main
+
+mask:	dc.b $b7,$1d,$c1,$04
+	dc.b $7e		; => seed+3 (last byte loaded)
+
+	;; table will not be loaded by monaload!
+brush::
+	dc.w 0x030A, 0x37BE, 0x2F9B, 0x072B, 0x0E3C, 0xF59B, 0x8A91, 0x1B0B
+	dc.w 0x0EBD, 0x9378, 0xB83E, 0xB05A, 0x70B5, 0x0280, 0xD0B1, 0x9CD2
+	dc.w 0x2093, 0x209C, 0x3D11, 0x26D6, 0xDF19, 0x97F5, 0x90A3, 0xA347
+	dc.w 0x8AF7, 0x0859, 0x29AD, 0xA32C, 0x7DFC, 0x0D7D, 0xD57A, 0x3051
+	dc.w 0xD431, 0x542B, 0xB242, 0xB114, 0x8A96, 0x2914, 0xB0F1, 0x532C
+	dc.w 0x0413, 0x0A09, 0x3EBB, 0xE916, 0x1877, 0xB8E2, 0xAC72, 0x80C7
+	dc.w 0x5240, 0x8D3C, 0x3EAF, 0xAD63, 0x1E14, 0xB23D, 0x238F, 0xC07B
+	dc.w 0xAF9D, 0x312E, 0x96CE, 0x25A7, 0x9E37, 0x2C44, 0x2BB9, 0x2139
+
+End:
+size	set End-Start
+
+	echo "Size:%dsize"
